2491312d1511386ddd792dadcc1c2d652f1da5af
pranavrav09 <71946217+pranavrav09@users.noreply.github.com>
2022-07-23 09:00:55 -0700

    Update ScheduleViewController.swift

--- ScheduleViewController.swift
+++ ScheduleViewController.swift
@@ -13,6 +13,24 @@ class ScheduleViewController: UIViewController, UITableViewDataSource, UITableVi
     var AssignmentDates: [Date] = []
     var CompletionTime: [Int] = []
     var ECTime: [Int] = []
+    var Monday: [String] = []
+    var Tuesday: [String] = []
+    var Wednesday: [String] = []
+    var Thursday: [String] = []
+    var Friday: [String] = []
+    var Saturday: [String] = []
+    var Sunday: [String] = []
+    
+    var MondayTime: [Int] = []
+    var TuesdayTime: [Int] = []
+    var WednesdayTime: [Int] = []
+    var ThursdayTime: [Int] = []
+    var FridayTime: [Int] = []
+    var SaturdayTime: [Int] = []
+    var SundayTime: [Int] = []
+    var AssignmentSchedule: [[String]] = []
+    var AssignmentTimes: [Int] = []
+    
     var totalElements = 0
     @IBOutlet weak var TableView: UITableView!
     var ref: DatabaseReference!
@@ -33,23 +51,34 @@ class ScheduleViewController: UIViewController, UITableViewDataSource, UITableVi
                     let dateFormatter = DateFormatter()
                     dateFormatter.dateStyle = DateFormatter.Style.short
                     self.AssignmentDates.append(dateFormatter.date(from: date)!)
+                    self.runAfterFirebase()
                 })
+                
             }
-            for i in 0..<self.totalElements{
-                self.createSchedule(currentIndex: i)
-            }
+            
         })
         
         
         
         // Do any additional setup after loading the view.
     }
+    func runAfterFirebase() {
+        for i in 0..<self.totalElements{
+            self.createSchedule(currentIndex: i)
+        }
+        self.separateDays()
+        self.TableView.dataSource = self
+        self.TableView.delegate = self
+        self.TableView.reloadData()
+    }
     func createSchedule(currentIndex: Int) {
         let dateFormatter = DateFormatter()
         let date = Date()
         dateFormatter.dateStyle = DateFormatter.Style.short
         let currentDate = dateFormatter.string(from: date)
+      
         let difference = Calendar.current.dateComponents([.day], from: dateFormatter.date(from: currentDate)!, to: AssignmentDates[currentIndex])
+
         var daysDifference = difference.day
         let initialDaysDifference = daysDifference! - 1
         var timePerDay = Double(CompletionTime[currentIndex])/Double(daysDifference!)
@@ -60,14 +89,106 @@ class ScheduleViewController: UIViewController, UITableViewDataSource, UITableVi
             
         }
         let spaceBetweenStudying = initialDaysDifference/daysDifference!
+        //works till here
         let schedule = daysToStudy(space: spaceBetweenStudying, currentDate: currentDate, daysDifference: daysDifference!)
+        print(schedule)
         var scheduleDOW: [String] = []
         for i in schedule {
             let dateFormatter = DateFormatter()
             dateFormatter.dateFormat = "EEEE"
             scheduleDOW.append(dateFormatter.string(from: i))
         }
+        
+        var daysOfWeek = convertDaysOfWeek(daysOfWeek: scheduleDOW)
+        daysOfWeek.insert(AssignmentNames[currentIndex], at: 0)
+        
+        AssignmentSchedule.append(daysOfWeek)
+        AssignmentTimes.append(Int(timePerDay))
+        print(AssignmentTimes)
+        print(AssignmentSchedule)
+        print(AssignmentNames)
+        print(AssignmentDates)
     }
+    func separateDays() {
+        for i in 0..<AssignmentSchedule.count{
+            for j in 0..<AssignmentSchedule[i].count {
+                if AssignmentSchedule[i][j] == "Monday"{
+                    
+                    self.Monday.append(AssignmentSchedule[i][0])
+                    self.MondayTime.append(AssignmentTimes[i])
+                }
+                if AssignmentSchedule[i][j] == "Tuesday"{
+                    self.Tuesday.append(AssignmentSchedule[i][0])
+                    self.TuesdayTime.append(AssignmentTimes[i])
+                }
+                if AssignmentSchedule[i][j] == "Wednesday"{
+                    self.Wednesday.append(AssignmentSchedule[i][0])
+                    self.WednesdayTime.append(AssignmentTimes[i])
+                }
+                if AssignmentSchedule[i][j] == "Thursday"{
+                    self.Thursday.append(AssignmentSchedule[i][0])
+                    self.ThursdayTime.append(AssignmentTimes[i])
+                }
+                if AssignmentSchedule[i][j] == "Friday"{
+                    self.Friday.append(AssignmentSchedule[i][0])
+                    self.FridayTime.append(AssignmentTimes[i])
+                }
+                if AssignmentSchedule[i][j] == "Saturday"{
+                    self.Saturday.append(AssignmentSchedule[i][0])
+                    self.SaturdayTime.append(AssignmentTimes[i])
+                }
+                if AssignmentSchedule[i][j] == "Sunday"{
+                    self.Sunday.append(AssignmentSchedule[i][0])
+                    self.SundayTime.append(AssignmentTimes[i])
+                }
+                
+            }
+        }
+    }
+    func convertDaysOfWeek(daysOfWeek: [String]) -> [String]{
+        var weekArray: [String] = []
+        
+        for i in 0..<daysOfWeek.count{
+            if i == 0{
+                continue
+            }
+            else if daysOfWeeksNumber(currentDay: daysOfWeek[i]) > daysOfWeeksNumber(currentDay: daysOfWeek[i-1]){
+                weekArray.append(daysOfWeek[i])
+           
+            }
+            else {
+                break
+            }
+            
+            
+        }
+        return weekArray
+    }
+    func daysOfWeeksNumber(currentDay: String) -> Int{
+        if currentDay == "Sunday"{
+            return 1
+        }
+        else if currentDay == "Monday"{
+            return 2
+        }
+        else if currentDay == "Tuesday"{
+            return 3
+        }
+        else if currentDay == "Wednesday"{
+            return 4
+        }
+        else if currentDay == "Thursday"{
+            return 5
+        }
+        else if currentDay == "Friday"{
+            return 6
+        }
+        else if currentDay == "Saturday"{
+            return 7
+        }
+        return 0
+    }
+   
     func daysToStudy(space: Int, currentDate: String, daysDifference: Int) -> [Date]{
         
         let dateFormatter = DateFormatter()
@@ -81,12 +202,6 @@ class ScheduleViewController: UIViewController, UITableViewDataSource, UITableVi
         }
         var allDates: [Date] = []
         intDay+=1
-        dateFormatter.dateFormat = "dd"
-        let day = dateFormatter.string(from: date!)
-        var intDay = 0
-        if let number = Int(day){
-            intDay = number
-        }
         dateFormatter.dateFormat = "MM"
         let month = dateFormatter.string(from: date!)
         var intMonth = 0
@@ -149,10 +264,11 @@ class ScheduleViewController: UIViewController, UITableViewDataSource, UITableVi
         return 7
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return 0
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        var cell = UITableViewCell()
+        return cell
     }
     /*
     // MARK: - Navigation
